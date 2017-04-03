984f33ea0c8b231715aa275fe05828602942f217
Nghia Tran <vinhnghiatran@gmail.com>
2017-04-03 15:16:02 +0700

    init workspace, titan_macOS, titanCore

---
+++ Titan_macOS/Titan_macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Titan_macOS
+//
+//  Created by Nghia Tran on 4/3/17.
+//  Copyright © 2017 nghiatran. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Titan_macOS/Titan_macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Titan_macOS
+//
+//  Created by Nghia Tran on 4/3/17.
+//  Copyright © 2017 nghiatran. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
